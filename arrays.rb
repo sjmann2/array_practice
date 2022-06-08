efcdce8c2a562db521238ac5572b5fee38f058ba
Sid Mann <sjmann2@outlook.com>
2022-06-08 11:20:04 -0500

    Add methods

--- arrays.rb
+++ arrays.rb
@@ -1,4 +1,12 @@
 tea_types = ["earl gray", "black", "chai", "green"]
+#push will add an additional string ("sleepytime") to the end of the array
+tea_types.push ("sleepytime")
 cups_drank = [0, 1, 2, 3, 5]
+#pop will remove and return the last integer (5) from the array
+cups_drank.pop
 cost_gas = [5.99, 6.25, 7.19, 5.55]
+#shift will remove and return the first float (5.99) from the array
+cost_gas.shift
 owns_car = [true, true, false, true]
+#unshift will add an additional boolean (false) to the front of the array
+owns_car.unshift(false)

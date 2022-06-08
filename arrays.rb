f236740acb01fb500e9e2cb7d5e81f35a2ee0cb7
Sid Mann <sjmann2@outlook.com>
2022-06-08 11:23:37 -0500

    Add index position

--- arrays.rb
+++ arrays.rb
@@ -1,11 +1,15 @@
 tea_types = ["earl gray", "black", "chai", "green"]
 #push will add an additional string ("sleepytime") to the end of the array
+#"earl gray" index position is 0, "black" is 1, "chai" is 2, and "green" is 3
+
 tea_types.push ("sleepytime")
 cups_drank = [0, 1, 2, 3, 5]
 #pop will remove and return the last integer (5) from the array
+#5 is in index position 4
 cups_drank.pop
 cost_gas = [5.99, 6.25, 7.19, 5.55]
 #shift will remove and return the first float (5.99) from the array
+#6.25 is in index position 1
 cost_gas.shift
 owns_car = [true, true, false, true]
 #unshift will add an additional boolean (false) to the front of the array
